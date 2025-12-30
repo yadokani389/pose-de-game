8377aaf980982b65926e78ad76330da72b5c4263
yadokani389 <yadokani389@gmail.com>
2025-12-30 21:45:01 +0900

    chore: add nix cache

--- flake.nix
+++ flake.nix
@@ -189,4 +189,15 @@
           };
         };
     };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://yadokani389.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "yadokani389.cachix.org-1:xHw9jijQFNDKlNprHbQpXX6cVOUO4m/n2lBfx6Bq4jg="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 }
